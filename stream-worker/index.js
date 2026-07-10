4e0f09323905c23206704e26e42fa914e57337e3
arkhan66648 <rmadskhanfirst4323@gmail.com>
2026-07-10 17:02:56 +0600

    Add Histats tracking codes
    - Main site (ID: 5038558) in Base.astro <head> — covers all pages
    - Watch subdomain (ID: 5038559) in stream-worker html() <head> — covers
      all streaming pages and error pages

--- stream-worker/index.js
+++ stream-worker/index.js
@@ -63,6 +63,18 @@ function html(body) {
 <meta name="robots" content="noindex, nofollow">
 <title>Buffstreams Backup</title>
 <style>${STYLES}</style>
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,5038559,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5038559&101" alt="free webpage hit counter" border="0"></a></noscript>
+<!-- Histats.com  END  -->
 </head>
 <body>
 ${body}
